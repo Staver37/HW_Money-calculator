09524eee568c4203703ac4cd96655694934395be
Staver37 <102354895+Staver37@users.noreply.github.com>
2022-03-25 21:31:20 +0200

    Add files via upload

---
+++ HW_money-calculator.py
@@ -0,0 +1,18 @@
+#HW money calculator
+# money calculator
+
+#INS:
+
+# * amount of money $
+# * bills (1,5,20)
+# OUT
+# * number of bill
+
+money = int ( input("Enter $: ") ) # int
+
+bills_20 = money // 20 # int
+bills_5 = money // 5 % bills_20
+bills_1 = money % 5
+
+print (bills_20, "x20+" , bills_5, "x $5" , bills_1, "x $1" )
+ 
